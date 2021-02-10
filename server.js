79e26d9df90ebf135bd5c074b7b7123564f9c497
dfilitto <danilo.filitto@gmail.com>
2021-02-10 10:54:01 -0300

    'teste2'

--- server.js
+++ server.js
@@ -6,9 +6,11 @@ const middlewares = jsonServer.defaults();
 
 const port = process.env.PORT || 8080;
 
-server.use(middlewares);
-server.use(jsonServer.bodyParser);
-server.use(router);
+//server.use(middlewares);
+server.use('/api/v1', middlewares)
+server.use(jsonServer.bodyParser); //coloquei parar ver se mantém os dados
+server.use('/api/v1', router)
+//server.use(router);
 server.listen(port, () => {
   // eslint-disable-next-line no-console
   console.log(`JSON Server is running in ${port}`);
